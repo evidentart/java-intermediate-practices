24a5509058ab3dda381ef66f192aa98400bbf7dd
Ali Akcin <92172480+evidentart@users.noreply.github.com>
2024-03-24 16:19:53 -0400

    Create RepairBot.java

---
+++ robot/src/Model/RepairBot.java
@@ -0,0 +1,14 @@
+package Model;
+
+public RepairBot(){
+  setTool("welding Torch");
+  setPowerlevel(10);
+
+public void recharge(){
+  System.out.println("Clank, boom, kerchunk! Repair bot now plugged in");
+  System.out.println("Power level " + getPowerlevel());
+}
+  public void doWork(){
+    System.out.println("Repair bot uses it's" + getTool() + " and repairs the nuclear power plant.");
+  }
+}
