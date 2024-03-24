67777c0338cf8167f9f8fd573bf8e4fcb1c55011
Ali Akcin <92172480+evidentart@users.noreply.github.com>
2024-03-24 16:27:07 -0400

    Create Robots.java

---
+++ robot/src/Model/Robots.java
@@ -0,0 +1,27 @@
+// package Model;
+
+// public abstract class Robots {
+  
+//   private String tool;
+//   private int powerlevel;
+
+// public abstract void recharge();
+// public abstract void doWork();
+
+// public String getTool() {
+//   return tool;
+// }
+
+// public void setTool(String tool) {
+//   this.tool = tool;
+// }
+
+// public int getPowerlevel() {
+//   return powerlevel;
+// }
+
+// public void setPowerlevel(int powerlevel) {
+//   this.powerlevel = powerlevel;
+// }
+
+// }
